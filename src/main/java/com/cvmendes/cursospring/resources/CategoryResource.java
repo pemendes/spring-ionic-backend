0e1772fe96d1652a03d7434c1ba2ad0909cb0176
Miqeas Pedo MENDES <miqeasmendes@gmail.com>
2020-04-19 17:32:11 +0200

    Teste de première classe model: Category

--- src/main/java/com/cvmendes/cursospring/resources/CategoryResource.java
+++ src/main/java/com/cvmendes/cursospring/resources/CategoryResource.java
@@ -1,16 +1,29 @@
 package com.cvmendes.cursospring.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.cvmendes.cursospring.model.Category;
 
 @RestController
 @RequestMapping(value="/categories")
 public class CategoryResource {
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String start() {
-		return "Rest fonctionne bien";
+	public List<Category> start() {
+		
+		Category cat1 = new Category(1, "informatique");
+		Category cat2 = new Category(2, "Electronic");
+		
+		List<Category> list = new ArrayList<>();
+		list.add(cat1);
+		list.add(cat2);
+			
+		return list;
 	}
 
 }
