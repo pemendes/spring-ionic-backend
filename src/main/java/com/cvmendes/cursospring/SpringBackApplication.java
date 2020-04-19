51d26d1dbafe5a2f9e5250266ee5c3159c269cc8
Miqeas Pedo MENDES <miqeasmendes@gmail.com>
2020-04-19 19:27:17 +0200

    Opération d'instanciation

--- src/main/java/com/cvmendes/cursospring/SpringBackApplication.java
+++ src/main/java/com/cvmendes/cursospring/SpringBackApplication.java
@@ -1,13 +1,34 @@
 package com.cvmendes.cursospring;
 
+
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.cvmendes.cursospring.domain.Category;
+import com.cvmendes.cursospring.repositories.CategoryRepository;
+
 @SpringBootApplication
-public class SpringBackApplication {
+public class SpringBackApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoryRepository categoryRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBackApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Category cat1 = new Category(null, "Informatique");
+		Category cat2 = new Category(null, "Bureautique");
+		
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2));		
 	}
 
 }
