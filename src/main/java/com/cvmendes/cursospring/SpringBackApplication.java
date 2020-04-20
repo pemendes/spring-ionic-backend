783f7ea7f63ee61619d5e698ea73ebcdc510eba4
Miqeas Pedo MENDES <miqeasmendes@gmail.com>
2020-04-20 21:08:48 +0200

    State and City

--- src/main/java/com/cvmendes/cursospring/SpringBackApplication.java
+++ src/main/java/com/cvmendes/cursospring/SpringBackApplication.java
@@ -10,9 +10,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.cvmendes.cursospring.domain.Category;
+import com.cvmendes.cursospring.domain.City;
 import com.cvmendes.cursospring.domain.Product;
+import com.cvmendes.cursospring.domain.State;
 import com.cvmendes.cursospring.repositories.CategoryRepository;
+import com.cvmendes.cursospring.repositories.CityRepository;
 import com.cvmendes.cursospring.repositories.ProductRepository;
+import com.cvmendes.cursospring.repositories.StateRepository;
 
 @SpringBootApplication
 public class SpringBackApplication implements CommandLineRunner {
@@ -21,6 +25,10 @@ public class SpringBackApplication implements CommandLineRunner {
 	private CategoryRepository categoryRepository;
 	@Autowired
 	private ProductRepository productRepository;
+	@Autowired
+	private CityRepository cityRepository;
+	@Autowired
+	private StateRepository stateRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBackApplication.class, args);
@@ -45,6 +53,18 @@ public class SpringBackApplication implements CommandLineRunner {
 		
 		categoryRepository.saveAll(Arrays.asList(cat1, cat2));
 		productRepository.saveAll(Arrays.asList(p1, p2, p3));
+		
+		State st1 = new State(null, "Minas Gerais");
+		State st2 = new State(null, "São Paulo");
+		
+		City c1 = new City(null, "Uberlândia", st1);
+		City c2 = new City(null, "São Paulo", st2);
+		City c3 = new City(null, "Campinas", st2);
+		
+		st1.getCities().addAll(Arrays.asList(c1));
+		st2.getCities().addAll(Arrays.asList(c2, c3));
+		
+		stateRepository.saveAll(Arrays.asList(st1, st2));
+		cityRepository.saveAll(Arrays.asList(c1, c2, c3));
 	}
-
 }
---
+++ src/main/java/com/cvmendes/cursospring/repositories/StateRepository.java
@@ -0,0 +1,11 @@
+package com.cvmendes.cursospring.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.cvmendes.cursospring.domain.State;
+
+@Repository
+public interface StateRepository extends JpaRepository<State, Integer> {
+
+}
