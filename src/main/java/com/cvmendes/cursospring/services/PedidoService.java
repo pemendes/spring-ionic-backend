28faed589396782dc6448aa1c46bee068e933fa5
Miqeas Pedo MENDES <miqeasmendes@gmail.com>
2020-04-23 02:05:45 +0200

    Endpoint /pedidos/{id} disponible

---
+++ src/main/java/com/cvmendes/cursospring/services/PedidoService.java
@@ -0,0 +1,24 @@
+package com.cvmendes.cursospring.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.cvmendes.cursospring.domain.Pedido;
+import com.cvmendes.cursospring.repositories.PedidoRepository;
+import com.cvmendes.cursospring.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+
+	@Autowired
+	private PedidoRepository repo;
+
+	public Pedido buscar(Integer id) {
+		Optional<Pedido> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
+	}
+
+}
