86346a3bcab9a76871f93283bd6c7811201474ef
Miqeas Pedo MENDES <miqeasmendes@gmail.com>
2020-04-25 04:09:46 +0200

    Validation customisée: email non répeté dans l'insertion de client 'Cliente'

--- src/main/java/com/cvmendes/cursospring/repositories/ClienteRepository.java
+++ src/main/java/com/cvmendes/cursospring/repositories/ClienteRepository.java
@@ -2,10 +2,13 @@ package com.cvmendes.cursospring.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.cvmendes.cursospring.domain.Cliente;
 
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
 
+	@Transactional(readOnly = true)
+	Cliente findByEmail(String email);
 }
