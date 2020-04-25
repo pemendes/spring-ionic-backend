d306b5df0cbbe7d0bd1bb43eb240fc2cb2c4b40d
Miqeas Pedo MENDES <miqeasmendes@gmail.com>
2020-04-25 02:50:44 +0200

    Supprimer une catégorie 'Categoria' avec DELETE

--- src/main/java/com/cvmendes/cursospring/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/cvmendes/cursospring/resources/exceptions/ResourceExceptionHandler.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.cvmendes.cursospring.services.exceptions.DataIntegrityException;
 import com.cvmendes.cursospring.services.exceptions.ObjectNotFoundException;
 
 @ControllerAdvice
@@ -14,10 +15,16 @@ public class ResourceExceptionHandler {
 
 	@ExceptionHandler(ObjectNotFoundException.class)
 	public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request) {
-		String message = e.getMessage();
-		Integer status = HttpStatus.NOT_FOUND.value();
-		StandardError err = new StandardError(status, message, System.currentTimeMillis());
-		return ResponseEntity.status(status).body(err);
+
+		StandardError err = new StandardError(HttpStatus.NOT_FOUND.value(), e.getMessage(), System.currentTimeMillis());
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
 	}
-	
+
+	@ExceptionHandler(DataIntegrityException.class)
+	public ResponseEntity<StandardError> dataIntegrity(DataIntegrityException e, HttpServletRequest request) {
+
+		StandardError err = new StandardError(HttpStatus.BAD_REQUEST.value(), e.getMessage(),
+				System.currentTimeMillis());
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
+	}
 }
