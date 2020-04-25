cdc3dbb483fed0abc056f92dd4f9794b35c80c9c
Miqeas Pedo MENDES <miqeasmendes@gmail.com>
2020-04-25 04:31:07 +0200

    Recherche des produits par nom et catégories

--- src/main/java/com/cvmendes/cursospring/repositories/ProdutoRepository.java
+++ src/main/java/com/cvmendes/cursospring/repositories/ProdutoRepository.java
@@ -1,11 +1,26 @@
 package com.cvmendes.cursospring.repositories;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.cvmendes.cursospring.domain.Categoria;
 import com.cvmendes.cursospring.domain.Produto;
 
 @Repository
 public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
+
+	// Utiliser l'annotation Query quand le nom de la méthode ne respecte pas les
+	// normes de methods names de Jpa
+	@Transactional(readOnly = true)
+	@Query("SELECT DISTINCT obj FROM Produto obj INNER JOIN obj.categorias cat WHERE obj.nome LIKE %:nome% AND cat IN :categorias")
+	Page<Produto> findDistinctByNomeContainingAndCategoriasIn(@Param("nome") String nome,
+			@Param("categorias") List<Categoria> categorias, Pageable pageRequest);
 
 }
