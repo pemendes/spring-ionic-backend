7c83100d736c1ff979f5ae5a035386909e9188a1
Miqeas Pedo MENDES <miqeasmendes@gmail.com>
2020-04-25 04:01:57 +0200

    Validation customizée de CPF ou CNPJ dans l'insertion de client 'Cliente'

---
+++ src/main/java/com/cvmendes/cursospring/services/validation/ClienteInsertValidator.java
@@ -0,0 +1,40 @@
+package com.cvmendes.cursospring.services.validation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import com.cvmendes.cursospring.domain.enums.TipoCliente;
+import com.cvmendes.cursospring.dto.ClienteNewDTO;
+import com.cvmendes.cursospring.resources.exceptions.FieldMessage;
+import com.cvmendes.cursospring.services.validation.utils.BR;
+
+public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+
+	@Override
+	public void initialize(ClienteInsert ann) {
+	}
+
+	@Override
+	public boolean isValid(ClienteNewDTO objDto, ConstraintValidatorContext context) {
+
+		List<FieldMessage> list = new ArrayList<>();
+
+		if (objDto.getTipo().equals(TipoCliente.PESSOAFISICA.getCod()) && !BR.isValidCPF(objDto.getCpfOuCnpj())) {
+			list.add(new FieldMessage("cpfOuCnpj", "CPF inválido"));
+		}
+
+		if (objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
+			list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
+		}
+
+		for (FieldMessage e : list) {
+			context.disableDefaultConstraintViolation();
+			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
+					.addConstraintViolation();
+		}
+		return list.isEmpty();
+	}
+}
