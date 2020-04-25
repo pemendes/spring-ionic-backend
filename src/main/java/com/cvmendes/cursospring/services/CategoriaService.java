e498f69470b5a2c8f92aee8f58e1b005d615e500
Miqeas Pedo MENDES <miqeasmendes@gmail.com>
2020-04-25 03:15:22 +0200

    Validation syntastique avec Bean Validation

--- src/main/java/com/cvmendes/cursospring/services/CategoriaService.java
+++ src/main/java/com/cvmendes/cursospring/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.cvmendes.cursospring.domain.Categoria;
+import com.cvmendes.cursospring.dto.CategoriaDTO;
 import com.cvmendes.cursospring.repositories.CategoriaRepository;
 import com.cvmendes.cursospring.services.exceptions.DataIntegrityException;
 import com.cvmendes.cursospring.services.exceptions.ObjectNotFoundException;
@@ -53,5 +54,9 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
+	}
+
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 }
